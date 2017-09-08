1af594a35c4c27e3f7c6a6e9ce64df0ff7daf38a
PRM <pawanrajmurarka@gmail.com>
2017-09-08 22:34:44 +0530

    Added migration for Neo entity

---
+++ app/DoctrineMigrations/Version20170908135709.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170908135709 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE Neo (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, neo_reference_id VARCHAR(255) DEFAULT NULL, speed VARCHAR(255) DEFAULT NULL, is_potentially_hazardous_asteroid TINYINT(1) DEFAULT \'0\' NOT NULL, date VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE Neo');
+    }
+}
